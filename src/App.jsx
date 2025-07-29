e5a1232d50e4118edc15b86f8a8945a7aedb2692
Deepak Singh <deepaksingh927@gmail.com>
2025-07-29 21:07:07 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
-import React from "react";
+ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"; // Make sure the path is correct
+import Navbar from "./components/Navbar";
 import HeroSection from "./pages/HeroSection";
 import VideoSection from "./pages/VideoSection";
 import WhatWeDo from "./components/WhatWeDo";
 import BrandExperience from "./components/BrandExperience";
 import PricingPlans from "./components/PricingPlans";
-import DoubleSlider from "./components/DoubleSlider"; 
-import  ControlledVideoSwiper from "./components/Testimonials"; 
+import DoubleSlider from "./components/DoubleSlider";
+import ControlledVideoSwiper from "./components/Testimonials";
 import FAQComponent from "./components/FAQComponent";
 import PixelNestFooter from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      
       <Routes>
+        {/* Default route */}
+        <Route path="/" element={<HeroSection />} />
+
+        {/* Other routes */}
         <Route path="/navbar" element={<Navbar />} />
         <Route path="/herosection" element={<HeroSection />} />
         <Route path="/videosection" element={<VideoSection />} />
         <Route path="/whatwedo" element={<WhatWeDo />} />
         <Route path="/brandexperience" element={<BrandExperience />} />
-        <Route path="/PricingPlans" element={<PricingPlans />} />
+        <Route path="/pricingplans" element={<PricingPlans />} />
         <Route path="/doubleslider" element={<DoubleSlider />} />
-        <Route path="/controlledvideoswiper" element={< ControlledVideoSwiper />} />
+        <Route path="/controlledvideoswiper" element={<ControlledVideoSwiper />} />
         <Route path="/faqcomponent" element={<FAQComponent />} />
-        <Route path="/ pixelnestfooter" element={< PixelNestFooter />} />
-
- 
-
-
-
-
-
-        {/* <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/case-study" element={<CaseStudy />} />
-          <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/pixelnestfooter" element={<PixelNestFooter />} />
       </Routes>
     </Router>
   );
